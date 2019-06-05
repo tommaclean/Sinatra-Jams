79366c8ab21ce009ff5f76a6ad6f7c4a01b1c49c
tommaclean <tom.j.maclean@gmail.com>
2019-06-05 15:20:40 -0400

    connect application controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,23 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+    get "/artists" do
+      erb :index
+    end
+
+    get "/artists/new" do
+      erb :new
+    end
+
+    post "/artists" do
+      @artist = Artist.create(params)
+      redirect to "/artists/#{@artist.id}"
+    end
+
+    get "/artists/:id" do
+      @artist = Artist.find_by(id: params[:id])
+      erb :show
+    end
+
+
 end
---
+++ db/migrate/20190605183508_artists_table.rb
@@ -0,0 +1,9 @@
+class ArtistsTable < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.integer :age
+      t.string :bio
+    end
+  end
+end
